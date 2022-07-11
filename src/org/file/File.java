576f65a806625a3ca7a75eaa25be03a5d7aea5ed
ajeethavimal <ajeethamani92@gmail.com>
2022-07-11 12:24:08 +0530

    new code added

---
+++ src/org/file/File.java
@@ -0,0 +1,15 @@
+package org.file;
+public class File {
+public static void main(String[] args) {
+	int a=10,b=20,c=a;
+	
+	System.out.println("Before Swapping");
+	System.out.println("a is"+a);
+	System.out.println("b is"+b);
+	a=b;
+	b=c;
+	System.out.println("a is"+a);
+	System.out.println("b is"+b);
+	
+}
+}
